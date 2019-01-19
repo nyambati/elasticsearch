4bff12f1ebe432f2ae45883f78cb29587b490459
Nyambati Thomas <thomas.nyambati@andela.com>
2019-01-19 21:44:38 +0300

    add dynamic header for the table
    Signed-off-by: Nyambati Thomas <thomas.nyambati@andela.com>

--- bin/render.js
+++ bin/render.js
@@ -1,37 +1,24 @@
 const Table = require('cli-table2');
 
-const results = new Table({
-  head: [
-    ' ',
-    'Passenger Id',
-    'Survived',
-    'Pclass',
-    'Passenger Name',
-    'Sex',
-    'Age',
-    'SibSp',
-    'Parch',
-    'Ticket',
-    'Fare',
-    'Cabin',
-    'Embarked'
-  ]
-});
-
-const indices = new Table({
-  head: [' ', 'indices'],
-  colWidths: [5, 35]
-});
+const hasNoData = data => {
+  if (data.length > 0) return false;
+  console.log();
+  console.log('No results to display');
+  console.log();
+  return true;
+};
 
 const renderResultsTable = (data, totalResults) => {
-  if (data.length === 0) {
-    console.log('================== No results to display =================');
-    return;
-  }
+  if (hasNoData(data)) return;
+
+  const head = [' ', ...Object.keys(data[0]._source)];
+
+  const results = new Table({ head });
 
   for (let index in data) {
     results.push([Number(index) + 1, ...Object.values(data[index]._source)]);
   }
+
   console.log(results.toString());
   console.log();
   console.log('TOTAL RESULTS FOUND: ', totalResults);
@@ -39,10 +26,7 @@ const renderResultsTable = (data, totalResults) => {
 };
 
 const renderIndicesTable = data => {
-  if (data.length <= 0) {
-    console.log('================== No results to display =================');
-    return;
-  }
+  if (hasNoData) return;
 
   data.forEach((index, i) => {
     indices.push([Number(i) + 1, index]);
